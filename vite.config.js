7fcf6dea99769991a073e3d34e82b22b13a26ffb
FSJorgeGC <garciajcarpio@alumnos.cei.es>
2025-07-19 13:15:59 +0200

    Instalado sass y limpiado entorno para evitar errores de node-sass

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base:"./",
   plugins: [react()],
-  root: './src',  
   build: {
     outDir: '../dist',
     emptyOutDir: true,
