9fc25b9064faa2a55741b85d014eb3f87fa9d286
FSJorgeGC <garciajcarpio@alumnos.cei.es>
2025-07-19 12:50:51 +0200

    Commit para subida de web

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
   base:"./",
   plugins: [react()],
+  root: './src',  
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
