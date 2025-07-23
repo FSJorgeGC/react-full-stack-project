8b102b21504a1add4597c6a8448b92fcb3856b6d
FSJorgeGC <garciajcarpio@alumnos.cei.es>
2025-07-23 11:10:00 +0200

    cambios

--- backend/index.js
+++ backend/index.js
@@ -2,24 +2,36 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import indexRoutes from "./routes/index.routes.js"; // o "./routes/auth.routes.js" si quieres ser más específico
-
+import indexRoutes from "./routes/index.routes.js";
 
 dotenv.config();
 
 const app = express();
-app.use(cors({origin: process.env.FRONTEND_URL || "http://localhost:5173" // Cambia esto según tu entorno
+
+// Lista de orígenes permitidos
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://eloquent-starlight-bd56f5.netlify.app"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Permitir peticiones sin origin (como Postman)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("❌ Not allowed by CORS: " + origin));
+    }
+  },
+  credentials: true
 }));
+
 app.use(express.json());
 
-
 app.use("/api/v1", indexRoutes);
-
-
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Mongo conectado"))
   .catch(err => console.error("❌ Error Mongo:", err));
 
 app.listen(3000, () => console.log("🚀 Backend en http://localhost:3000"));
-
