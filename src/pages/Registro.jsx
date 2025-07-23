7c94916540cc37d09a88933422c704729b291809
FSJorgeGC <garciajcarpio@alumnos.cei.es>
2025-07-23 11:32:19 +0200

    cambios

--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -13,7 +13,7 @@ import { useNavigate } from "react-router-dom";
 
 
 export const Registro = () => {
-    const BACKEND_API = import.meta.env.VITE_BACKEND_URL;
+    const BACKEND_API = import.meta.env.VITE_API_URL;
     const [email, setEmail] = useState("");
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
